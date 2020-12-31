3cb2d97860e2443bd60029b4b78b8ce271d4bd34
gitUmaru <umaru.alia@gmail.com>
2020-12-30 16:21:52 -0800

    made util a directory + moved requirements txt

--- setup.py
+++ setup.py
@@ -18,4 +18,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+       "biopython",
+       "uniprot",
+       "requests",
+       "matplotlib",
+       "beautifulsoup4"
+   ]
 )
